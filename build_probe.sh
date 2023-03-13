258d83480ee93712d692b3b0a814a9f22b20463e
Guillaume Corré <gcorre@gatling.io>
2023-03-13 12:10:32 +0100

    feat: switch to zulu, add latest publish feat

--- build_probe.sh
+++ build_probe.sh
@@ -3,21 +3,28 @@
 set -eux
 
 java_major="$1"
+java_latest="$2"
 
-base_image="openjdk:${java_major}-jdk-slim"
+base_image="azul/zulu-openjdk:$java_major-jre-headless"
 
 docker pull "$base_image"
 
 java_version=$(docker run --rm "$base_image" java -version 2>&1 | awk -F '"' '/version/ {print $2}')
 
 docker build \
-  --build-arg=base_image="$base_image" \
-  -t gatlingcorp/classic-openjdk-x86:"$java_version" \
+  --build-arg=base_image=$base_image \
+  -t gatlingcorp/classic-openjdk-x86:$java_version \
   .
 
-docker tag \
-  gatlingcorp/classic-openjdk-x86:"$java_version" \
-  gatlingcorp/classic-openjdk-x86:"$java_major"
-
-docker push gatlingcorp/classic-openjdk-x86:"$java_version"
-docker push gatlingcorp/classic-openjdk-x86:"$java_major"
+if [ "$java_major" = "$java_latest" ]; then
+  docker tag \
+    gatlingcorp/classic-openjdk-x86:$java_version \
+    gatlingcorp/classic-openjdk-x86:latest
+  docker push gatlingcorp/classic-openjdk-x86:latest
+else
+  docker tag \
+    gatlingcorp/classic-openjdk-x86:$java_version \
+    gatlingcorp/classic-openjdk-x86:$java_major
+  docker push gatlingcorp/classic-openjdk-x86:$java_version
+  docker push gatlingcorp/classic-openjdk-x86:$java_major
+fi
